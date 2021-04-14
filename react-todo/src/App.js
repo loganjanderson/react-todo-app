1509454cec0b16e0c95f3ec53757ecba505701d2
Logan Anderson <logananderson@Logans-MBP.T-mobile.com>
2021-04-14 16:19:16 -0600

    Finished todo items and update state handlers

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,13 +1,48 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Form from "./components/form";
+import TodoList from "./components/todo-list";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setfilteredTodos] = useState([]);
+
+  useEffect(() => {
+    handleFilter();
+  }, [todos, status]);
+
+  const handleFilter = () => {
+    switch (status) {
+      case "completed":
+        setfilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "incompleted":
+        setfilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setfilteredTodos(todos);
+        break;
+    }
+  };
   return (
     <div className="App">
       <header>
         <h1>Logan's Todo List</h1>
       </header>
-      <Form />
+      <Form
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+        setStatus={setStatus}
+      />
+      <TodoList
+        setTodos={setTodos}
+        todos={todos}
+        filteredTodos={filteredTodos}
+      />
     </div>
   );
 }
--- react-todo/src/components/form.js
+++ react-todo/src/components/form.js
@@ -1,21 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Form extends Component {
-  render() {
-    return (
-      <form>
-        <input type="text" class="todo-input" />
-        <button class="todo-button" type="submit">
-          <i class="fas fa-plus-square"></i>
-        </button>
-        <div class="select">
-          <select name="todos" class="filter-todo">
-            <option value="all">All</option>
-            <option value="completed">Completed</option>
-            <option value="incomplete">Ready To Start</option>
-          </select>
-        </div>
-      </form>
-    );
-  }
-}
+const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
+  const handleInputText = (e) => {
+    console.log(e.target.value);
+    setInputText(e.target.value);
+  };
+
+  const handleSubmitTodo = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText("");
+  };
+
+  const handleStatus = (e) => {
+    setStatus(e.target.value);
+  };
+  return (
+    <form>
+      <input
+        value={inputText}
+        onChange={handleInputText}
+        type="text"
+        className="todo-input"
+      />
+      <button onClick={handleSubmitTodo} className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select onChange={handleStatus} name="todos" className="filter-todo">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="incomplete">Ready To Start</option>
+        </select>
+      </div>
+    </form>
+  );
+};
+export default Form;
